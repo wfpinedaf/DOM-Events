32896523948deeaf94da9d1f364b7131f05c2fdc
wfpinedaf <wfpinedaf@outlook.com>
2023-11-11 19:28:26 -0500

    update

--- front/script.js
+++ front/script.js
@@ -86,3 +86,33 @@ const handleHover = function (e) {
 nav.addEventListener('mouseover', handleHover.bind(0.5))
 
 nav.addEventListener('mouseout', handleHover.bind(1))
+
+// const initialCoords = section1.getBoundingClientRect()
+// console.log(initialCoords)
+
+// window.addEventListener('scroll', function () {
+//   console.log(window.scrollY)
+
+//   if (window.scrollY > initialCoords.top) nav.classList.add('sticky')
+//   else nav.classList.remove('sticky')
+// })
+
+//USING IntersectionObserver API
+const header = document.querySelector('.header')
+const navHeight = nav.getBoundingClientRect().height
+const stickyNav = function (entries) {
+  const [entry] = entries
+  console.log(entry)
+
+  if (!entry.isIntersecting) nav.classList.add('sticky')
+  else nav.classList.remove('sticky')
+}
+
+const headerObserver = new IntersectionObserver(
+  stickyNav, {
+  root: null,
+  threshold: 0,
+  rootMargin: `-${navHeight}px`
+}
+)
+headerObserver.observe(header)
